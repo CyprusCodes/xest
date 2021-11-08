62f930bab92c5c5f5ed02cf1dd999258b4793611
Ersel Aker <erselaker@gmail.com>
2021-11-08 20:52:15 +0000

    make db watch faster

--- cli/utils/runMySQLContainer.js
+++ cli/utils/runMySQLContainer.js
@@ -1,6 +1,7 @@
 const chalk = require("chalk");
 const { execSync } = require("child_process");
 const path = require("path");
+const snakeCase = require("lodash/snakeCase");
 const runSqlQueryWithinContainer = require("./runSqlQueryWithinContainer");
 const resolvePortConflict = require("./resolvePortConflict");
 const sleep = require("./sleep");
@@ -54,26 +55,24 @@ const runMySQLContainer = async (rootPath, projectName) => {
   console.log(
     chalk.yellow`Waiting for MySQL Container to become ready. This should only take a few seconds.`
   );
-  while (!ready && retryCount < 10) {
-    const mySQLConnectionString = `mysql -h localhost -u root -ppassword -e 'status'`;
-    const checkDatabaseSchema = `docker exec -i ${mySQLContainerId} ${mySQLConnectionString}`;
-    ({ error, output } = await runSqlQueryWithinContainer(checkDatabaseSchema));
 
+  while (!ready && retryCount < 60) {
+    const query = `SELECT COUNT(*) as tbl_count FROM information_schema.tables WHERE table_schema = '${snakeCase(projectName)}_db';`
+    const mySQLConnectionString = `mysql -h localhost -u root -ppassword ${snakeCase(
+      projectName
+    )}_db`;
+    const checkDatabaseSchema = `docker exec -i ${mySQLContainerId} ${mySQLConnectionString} <<< "${query}"`;
+    ({ error, output } = await runSqlQueryWithinContainer(checkDatabaseSchema));
+      
     if (!error) {
-      const uptimeStatus = output
-        .split("\n")
-        .find(row => row.includes("Uptime"))
-        .replace(/\s/g, "")
-        .split(":")[1];
-      const uptimeRegex = /(\d*)sec$/;
-      const [_, secs] = uptimeStatus.match(uptimeRegex);
-      if (Number(secs) > 10 || uptimeStatus.includes("min")) {
+      const tableCount = output.replace(/\s/g, "").replace(/\D/g,'');
+      if (Number(tableCount) > 0 || Number(tableCount) === 0) {
         ready = true;
       }
     }
     retryCount++;
     if (!ready) {
-      await sleep(1000 * retryCount);
+      await sleep(300);
     }
   }
   if (!ready) {
