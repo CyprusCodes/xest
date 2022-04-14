fe9041bf8553a21643ade8aed39087ad63d9e6e0
Ersel Aker <hello@erselaker.com>
2022-04-15 00:57:58 +0300

    tagline

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
+  title: "Xest",
+  tagline: "REST APIs made easy",
   url: "https://your-docusaurus-test-site.com",
   baseUrl: "/",
   onBrokenLinks: "throw",
