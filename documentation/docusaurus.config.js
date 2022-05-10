a0d62a6153992a99d10b78fbe21615041cca03e0
Ersel Aker <hello@erselaker.com>
2022-05-10 20:06:12 +0300

    add favicon

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
+  favicon: "icon/favicon.ico",
   organizationName: "CyprusCodes", // Usually your GitHub org/user name.
   projectName: "xest", // Usually your repo name.
   deploymentBranch: "gh-pages",
