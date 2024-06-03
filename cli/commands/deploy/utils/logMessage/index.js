16e194088b388d1732ffe95d75faa85ec6550f53
RootViper4 <fracnkysourya.com>
2024-06-03 17:36:23 +0300

    update the link to see the docs

--- cli/commands/deploy/utils/logMessage/index.js
+++ cli/commands/deploy/utils/logMessage/index.js
@@ -1,5 +1,5 @@
 const successMessage = "Deployment files created successfully for Digital Ocean.";
-const detailsMessage = "For more details on deploying to Digital Ocean, including creating your droplet and more, visit: \x1b[34mhttps://franckyulquiorra.medium.com/deploy-xest-api-on-digital-ocean-a8f3820dbd37\x1b[0m";
+const detailsMessage = "For more details on deploying to Digital Ocean, including creating your droplet and more, visit: \x1b[34mhttps://xestjs.com/docs/digital-ocean\x1b[0m";
 
 
 module.exports = {
