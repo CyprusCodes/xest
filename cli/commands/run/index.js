f1e32ceed9b6d94edd4d880c3c2a6bef796903ae
Ammar <ammar.h.hashad@gmail.com>
2024-06-26 16:51:43 +0300

    show shutdown process event

--- cli/commands/run/index.js
+++ cli/commands/run/index.js
@@ -118,6 +118,7 @@ const run = async () => {
   ].forEach((eventType) => {
     process.on(eventType, (event) => {
       if (!isExiting) {
+        if (event) console.log(event);
         console.log(chalk.yellow`Stopping API and MySQL container.`);
         isExiting = true;
         exec(
